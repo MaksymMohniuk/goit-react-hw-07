7bbbf57432213830aaeea01d4255c1f0d2dd4ce9
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-05-03 00:15:07 +0300

    added-thynku

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,31 @@ import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm.jsx";
 import SearchBox from "./components/SearchBox/SearchBox.jsx";
 import ContactList from "./components/ContactList/ContactList.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchContacts } from "./redux/contactsOps.js";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage.jsx";
+import Loader from "./components/Loader/Loader.jsx";
 
 function App() {
+  const dispatch = useDispatch();
+  const fetchedContacts = useSelector((state) => state.contacts.items);
+  const isLoading = useSelector((state) => state.contacts.loading);
+  const isError = useSelector((state) => state.contacts.error);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <>
       <div>
         <h1 className="title">Phonebook</h1>
         <ContactForm />
         <SearchBox />
-        <ContactList />
+        {isLoading && <Loader />}
+        {isError && <ErrorMessage />}
+        {fetchedContacts !== null && <ContactList />}
       </div>
     </>
   );
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addContact, deleteContact, fetchContacts } from "./contactsOps";
 
 const INITIAL_STATE = {
   items: [],
@@ -10,23 +11,45 @@ const contactsSlice = createSlice({
   name: "contacts",
   // Початковий стан редюсера слайсу
   initialState: INITIAL_STATE,
-  // Об'єкт редюсерів
-  reducers: {
-    addContact(state, action) {
-      state.items.push(action.payload);
-    },
-    deleteContact(state, action) {
-      state.items = state.items.filter(
-        (contact) => contact.id !== action.payload
-      );
-    },
-  },
-});
 
-// Генератори екшенів
-export const { addContact, deleteContact } = contactsSlice.actions;
+  extraReducers: (builder) =>
+    builder.addCase(fetchContacts.pending, (state) => {
+      state.loading = true;
+      state.error = false;
+    }),
+})
+  .addCase(fetchContacts.fulfilled, (state, action) => {
+    state.loading = false;
+    state.items = action.payload;
+  })
+  .addCase(fetchContacts.rejected, (state) => {
+    state.loading = false;
+    state.error = true;
+  })
+  .addCase(addContact.pending, (state) => {
+    state.loading = true;
+    state.error = false;
+  })
+  .addCase(addContact.fulfilled, (state, action) => {
+    state.loading = false;
+    state.items = state.items.push(action.payload);
+  })
+  .addCase(addContact.rejected, (state) => {
+    state.loading = false;
+    state.error = true;
+  })
+  .addCase(deleteContact.pending, (state) => {
+    state.loading = true;
+    state.error = false;
+  })
+  .addCase(deleteContact.fulfilled, (state, action) => {
+    state.loading = false;
+    state.items = state.items.filter((item) => item.id !== action.payload);
+  })
+  .addCase(deleteContact.rejected, (state) => {
+    state.loading = false;
+    state.error = true;
+  });
 
 // Редюсер слайсу
 export const contactsReducer = contactsSlice.reducer;
-
-export const selectContacts = (state) => state.contacts.items;
