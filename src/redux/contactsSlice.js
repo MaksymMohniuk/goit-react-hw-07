c4eb4709f182687089be0be3732d0673cc92d0e8
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-05-03 22:49:36 +0300

    corrected-delete

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -47,7 +47,9 @@ const contactsSlice = createSlice({
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.loading = false;
-        state.items = state.items.filter((item) => item.id !== action.payload);
+        state.items = state.items.filter(
+          (item) => item.id !== action.payload.id
+        );
       })
       .addCase(deleteContact.rejected, (state) => {
         state.loading = false;
